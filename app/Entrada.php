1468f3e4e292c39c2c796d9155e112d874f58eff
Mayri Bautista <16041328@itdurango.edu.mx>
2020-06-16 20:51:35 -0500

    entrada

--- app/Entrada.php
+++ app/Entrada.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Entrada extends Model
 {
     protected $table = 'entradas';
+    protected $primaryKey = 'idEntrada';
     public $timestamps = false;
 }
